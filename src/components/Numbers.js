62731fa4b9cfc87c685ccdc812f783d9a410128e
JCLthatlittlerat <156462023+JCLthatlittlerat@users.noreply.github.com>
2025-02-06 16:05:07 +0300

    Update Numbers.js

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import OutputField from './OutputField';
 import InputField from './InputField';
-var result=0;
+var result = 10;
 function Numbers() {
   return (
     <div>
-        <OutputField props={result}/>
+        <OutputField props={result} />
         <InputField/>
         
     </div>
