27cb20650d653196e4a04bc51418c57e1a0d8f56
JCLthatlittlerat <156462023+JCLthatlittlerat@users.noreply.github.com>
2025-04-28 23:32:34 +0300

    foundation lyed for the second exercise of controlled-component

--- src/App.js
+++ src/App.js
@@ -4,33 +4,57 @@ import './App.css';
 
 // import NameFormUncontrolled from "./components/controlled-elements/NameFormUncontrolled.jsx";
 // import NameFormControlled from "./components/controlled-elements/NameFormControlled.jsx";
-import { useState } from 'react';
+// import { useState } from 'react'; //for the 'NameFormControlled';
 
 function App() {
-  const [name, setName] = useState("");
-  const handleSubmit = (e) => {
-    console.log("for submitted!")
-    e.preventDefault(); //this prevents the default behaivor of the DOM and let us control the state ourself instead controlling by the DOM.
-    setName("");
-  }
+
+
   return (
-    <div className="App">
-
-      <form onSubmit={handleSubmit}>
-        <h2>nameFormControlled</h2>
-        <fieldset>
-          <div className="Field">
-            <label htmlFor="name">Name: </label>
-            <input type="text" placeholder='Name' name='name' id='name' value={name} onChange={(e) => setName(e.target.value)} />
-          </div>
-          <button disabled={!name} type="submit">Submit</button>
-        </fieldset>
-
-      </form>
-
-
+    <div className='App'>
+      <div className='Field'>
+        <div>
+          <h2>FeedBack form</h2>
+        </div>
+        <input type='submit' value={'Submit'} className='submitBtn' />
+      </div>
     </div>
+
   );
+
+
+
+
+
+
+
+
+
+
+  //======== NameFormControlled ========
+  // const [name, setName] = useState("");
+  // const handleSubmit = (e) => {
+  //   console.log("for submitted!")
+  //   e.preventDefault(); //this prevents the default behaivor of the DOM and let us control the state ourself instead controlling by the DOM.
+  //   setName("");
+  // }
+  // return (
+  //   <div className="App">
+
+  //     <form onSubmit={handleSubmit}>
+  //       <h2>nameFormControlled</h2>
+  //       <fieldset>
+  //         <div className="Field">
+  //           <label htmlFor="name">Name: </label>
+  //           <input type="text" placeholder='Name' name='name' id='name' value={name} onChange={(e) => setName(e.target.value)} />
+  //         </div>
+  //         <button disabled={!name} type="submit">Submit</button>
+  //       </fieldset>
+
+  //     </form>
+
+
+  //   </div>
+  // );
 }
 
 export default App;
