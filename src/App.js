aa2c73888d98e6ed699b510846b4f67dbea628ad
JCLthatlittlerat <156462023+JCLthatlittlerat@users.noreply.github.com>
2025-04-29 23:30:54 +0300

    some minor changes are made

--- src/App.js
+++ src/App.js
@@ -15,6 +15,62 @@ function App() {
 
 
 
+  // ============ FeedBack Form version 1.0 ===========
+  // import React from 'react'
+  // import {useState} from 'react'
+  // return(
+  //     const [score, setScore] = useState('10');
+  //     const [comment, setComment] = useState("");
+  //     const handleSubmit = (e) => {
+  //       e.preventDefault();
+  //       if (Number(score) <= 5 && comment.length <= 10) {
+  //         alert("please provide a comment explaining why the experience was poor.")
+  //         return;
+  //       }
+  //       setComment("");
+  //       setScore("10");
+
+  //     }
+  //     return (
+  //       <div className='App'>
+  //         <form onSubmit={handleSubmit} className='formContainer'>
+  //           <fieldset>
+  //             <div className='Field'>
+  //               <div>
+  //                 <h2>FeedBack form</h2>
+  //                 <label htmlFor='inputRange'>Score: {score}⭐</label>
+  //                 <div  >
+  //                   <input
+  //                     className='inputRange'
+  //                     id='inputRange'
+  //                     type='range'
+  //                     min={0}
+  //                     max={10}
+  //                     value={score}
+  //                     onChange={(e) => setScore(e.target.value)}
+  //                   />
+
+  //                 </div>
+  //                 <div>
+  //                   <label htmlFor='textareaField'>Comment: </label>
+  //                   <div>
+  //                     <textarea value={comment} onChange={(e) => setComment(e.target.value)} className='textareaField' id='textareaField'></textarea>
+  //                   </div>
+  //                 </div>
+  //               </div>
+  //               <input type='submit' value={'Submit'} className='submitBtn' />
+  //             </div>
+  //             <p>Version 1.0</p>
+  //           </fieldset>
+  //         </form>
+  //       </div>
+  //     );
+  //   )
+
+
+
+
+
 
 
 
